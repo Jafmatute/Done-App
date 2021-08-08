6ed122d02f31ab386c10405ff59f3ac4f628c5e0
Jafmatute <josueari20@gmail.com>
2021-08-07 22:25:00 -0600

    Encapsulating Styles and Installing react-native-vectors-icons, ios-android configuration

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import {Button} from 'react-native-vector-icons/FontAwesome';
+
+//Screens
 import ViewImageScreen from './src/screens/ViewImageScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 
+//Components
+import AppText from './src/components/AppText';
+
 const App = () => {
-  return <WelcomeScreen />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Button name="facebook" backgroundColor="#3b5998">
+        Login with Facebook
+      </Button>
+    </SafeAreaView>
+  );
 };
 
 export default App;
@@ -12,5 +24,7 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    top: 30,
+    margin: 20,
   },
 });

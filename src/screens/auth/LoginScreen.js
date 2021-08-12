891198b8404045a18b3f05ed5c26f44c6ba0f324
Jafmatute <josueari20@gmail.com>
2021-08-12 13:06:41 -0600

    - Building Better Forms with Formik - Validation with Yup and Building the ErrorMessage Component

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,18 +1,30 @@
 import React, {useState} from 'react';
 import {Image, StyleSheet, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 import AppTextInput from '../../components/AppTextInput';
 import AppButton from '../../components/AppButton';
 import AppText from '../../components/AppText';
+import ErrorMessage from '../../components/ErrorMessage';
 import Screen from '../../components/Screen';
 
 import colors from '../../config/colors';
 
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('Ingrese una dirección de correo')
+    .email('Dirección de correo inválida')
+    .label('Email'),
+  password: Yup.string()
+    .required('Ingrese su contraseña')
+    .min(4, 'Debe ingresar 4 caracteres como mínimo')
+    .label('Password'),
+});
+
 const LoginScreen = () => {
   const logo = require('../../assets/logo-red.png');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [eyePassword, setEyePassword] = useState(true);
 
   return (
@@ -21,38 +33,46 @@ const LoginScreen = () => {
         <Image style={styles.logo} source={logo} />
         <AppText style={styles.text}>React Native </AppText>
       </View>
-      <AppTextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="email"
-        onChangeText={text => setEmail(text)}
-        keyboardType="email-address"
-        placeholder="Correo electronico"
-        textContentType="password"
-      />
-      <View>
-        <AppTextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          icon="lock"
-          onChangeText={text => setPassword(text)}
-          placeholder="Contraseña"
-          secureTextEntry={eyePassword ? true : false}
-          textContentType="password"
-        />
-        <Icon
-          onPress={() => setEyePassword(!eyePassword)}
-          style={styles.iconPass}
-          name={eyePassword ? 'eye-off' : 'eye'}
-          size={25}
-          color={eyePassword ? colors.medium : colors.secondary}
-        />
-      </View>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => console.log(values)}
+        validationSchema={validationSchema}>
+        {({handleChange, handleSubmit, errors}) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              onChangeText={handleChange('email')}
+              keyboardType="email-address"
+              placeholder="Correo electronico"
+              textContentType="password"
+            />
+            <ErrorMessage error={errors.email} />
+            <View>
+              <AppTextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="lock"
+                onChangeText={handleChange('password')}
+                placeholder="Contraseña"
+                secureTextEntry={eyePassword ? true : false}
+                textContentType="password"
+              />
+              <Icon
+                onPress={() => setEyePassword(!eyePassword)}
+                style={styles.iconPass}
+                name={eyePassword ? 'eye-off' : 'eye'}
+                size={25}
+                color={eyePassword ? colors.medium : colors.secondary}
+              />
+              <ErrorMessage error={errors.password} />
+            </View>
 
-      <AppButton
-        title="Inicio de Sesión"
-        onPress={() => console.log(email, password)}
-      />
+            <AppButton title="Inicio de Sesión" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 };

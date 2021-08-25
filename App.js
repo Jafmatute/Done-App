d38d936bd9464b2a33b909ec3cecdac3b19b0876
Jafmatute <josueari20@gmail.com>
2021-08-24 20:08:35 -0600

    add main repository

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import RegisterScreen from './src/screens/auth/RegisterScreen';
+import AccountScreen from './src/screens/AccountScreen';
 
 function App() {
-  return <RegisterScreen />;
+  return <AccountScreen />;
 }
 export default App;
